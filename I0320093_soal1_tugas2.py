6f12532e1fca3084416f3ea734b44855ef749db1
Sekar Salsabila <sekarsalsabilaaa@gmail.com>
2021-03-11 17:08:51 +0700

    update

--- I0320093_soal1_tugas2.py
+++ I0320093_soal1_tugas2.py
@@ -1,1 +1,25 @@
+#Menghitung Luas Persegi Panjang
+print("Menghitung Luas Persegi Panjang")
+P = float(input("Masukkan Panjang :"))
+L = float(input("Masukkan Lebar :"))
+Luas_Persegi_Panjang = (P * L)
+print("L :", Luas_Persegi_Panjang)
 
+#Menghitung Luas Lingkaran
+print("Menghitung Luas Lingkaran")
+Phi = 3.14
+R = float(input("Masukkan Jari-Jari :"))
+Luas_Lingkaran = (Phi ** R)
+print("L =", Luas_Lingkaran)
+
+#Menghitung Konversi Suhu Celcius ke Farenheit
+print("Menghitung Konversi Suhu Celcius ke Farenheit")
+C = float(input("Masukkan Suhu (Celcius) :"))
+Suhu_Farenheit = (9 * C / 5 + 32)
+print("F =", Suhu_Farenheit)
+
+#Menghitung Konversi Suhu Reamur ke Kelvin
+print("Menghitung Konversi Suhu Reamur ke Kelvin")
+R = float(input("Masukkan Suhu (Reamur) :"))
+Suhu_Kelvin = (5 * R / 4 + 273)
+print("K =", Suhu_Kelvin)
